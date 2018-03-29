dba7412f5500b1ef6ac1301dca93a966216e2c28
Yan Hankin <hankinyb@gmail.com>
2018-03-29 02:15:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+ const a = document.querySelector(selector)
+  
+  return a 
+}
+
+function nestedTarget() {
+  const b = document.getElementById('nested').querySelector('.target')
+  
+  return b
+}
+
+function deepestChild() {
+  const c = document.getElementById('grand-node').querySelector('div div div div div')
+  
+  return c 
+}
+
+function increaseRankBy(n) {
+  const d = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < d.length; i++) {
+    d[i].innerHTML = parseInt(d[i].innerHTML) + n
+  }
+}
